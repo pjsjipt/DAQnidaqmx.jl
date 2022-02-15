7a6ddb1b45cf7e2b7c9c1c39d32aacedf57320d9
Paulo José Saiz Jabardo <pjabardo@ipt.br>
2022-02-15 10:14:42 -0300

    Implemented basic funcionality

--- src/DAQnidaqmx.jl
+++ src/DAQnidaqmx.jl
@@ -1,23 +1,34 @@
 module DAQnidaqmx
 
 import AbstractDAQs
+using AbstractDAQs: AbstractDAQ, daqaddinput, daqstart, daqstop, daqread, daqacquire, daqconfig
+using AbstractDAQs: numchannels, daqchannels, isdaqfinished, isreading, samplesread
+
 import NIDAQ
-using NIDAQ: devices
-export NIDev, daqaddinput, daqstart, daqstop, daqread, daqacquire, daqconfig
-export numchannels, daqchannels
+
+export AbstractDAQ, NIDev, daqaddinput, daqstart, daqstop, daqread, daqacquire, daqconfig
+export numchannels, daqchannels, isdaqfinished, isreading, samplesread
+
 export NIException
 export TermConfig, NIDefault, NIRSE, NINRSE, NIDiff, NIPseudoDiff
 
+"Terminal configuration"
 @enum TermConfig::Int32 NIDefault=NIDAQ.DAQmx_Val_Cfg_Default NIRSE=NIDAQ.DAQmx_Val_RSE NINRSE=NIDAQ.DAQmx_Val_NRSE NIDiff=NIDAQ.DAQmx_Val_Diff NIPseudoDiff=NIDAQ.DAQmx_Val_PseudoDiff
 
 
 mutable struct NIDev <: AbstractDAQs.AbstractDAQ
     devname::String
     handle::NIDAQ.TaskHandle
-    NIDev(devname::String, handle::NIDAQ.TaskHandle) = new(devname, handle)
+    freq::Float64
+    nsamples::Int64
+    NIDev(devname::String, handle::NIDAQ.TaskHandle) = new(devname, handle, -1.0, -1)
 end
 
+"""
+`error_message(errnum, buflen)`
 
+Return an error/warning message from its code.
+"""
 function error_message(errnum, buflen=1000)
 
     buf = zeros(UInt8, buflen)
@@ -37,13 +48,31 @@ struct NIException <: Exception
     msg::String
 end
 
+"""
+`NIException(code)`
+
+Returns an `Exception` object specific for NIDAQmx. From a numeric code it 
+returns a description of the exception.
+"""
 function NIException(code)
     msg = error_message(code)
     return NIException(code, msg)
 end
 
-    
-function NIDev(devname::String="", loadmaxtask::Bool=false)
+
+"""
+`NIDev(devname, loadmaxtask=false)`
+
+Creates a device that handles NIDAQmx data acquisition. 
+In the parlance used by NIDAQmx, it creates a task.
+
+NI has tools such as NI MAX that can handle the creation and configuration of tasks. 
+With argument `loadmaxtask=true` these tasks can be used directly.
+
+The device name (`devname`) is the task name to be used by NIDAQmx.
+
+"""
+function NIDev(devname::String, loadmaxtask::Bool=false)
 
     th = Ref(NIDAQ.TaskHandle(0))
     if !loadmaxtask
@@ -59,7 +88,26 @@ function NIDev(devname::String="", loadmaxtask::Bool=false)
     return NIDev(devname, handle)
 end
 
+"""
+`stoptask(dev)`
+
+Stops the execution of a task. It calls NIDAQmx function `DAQmxStopTask`.
+"""
 function stoptask(dev::NIDev)
+
+    r = NIDAQ.DAQmxStopTask(dev.handle)
+
+    r != 0 && throw(NIException(r))
+    
+end
+
+"""
+`cleartask(dev)`
+
+Clears a task. It calls NIDAQmx function `DAQmxClearTask`. After calling this function, 
+the task must be reconfigured.
+"""
+function cleartask(dev::NIDev)
 
     r = NIDAQ.DAQmxClearTask(dev.handle)
 
@@ -67,11 +115,32 @@ function stoptask(dev::NIDev)
     
 end
 
-function cleartask(dev::NIDev)
 
+"""
+`daqstop(dev)`
+
+Just a wrapper around [`stoptask`](@ref) implementing the [`AbstractDAQs` ](@ref) interface.
+"""
+function AbstractDAQs.daqstop(dev::NIDev)
+    stoptask(dev)
 end
-function daqaddinput(dev::NIDev, chans::AbstractString, channames::AbstractString="";
-                     termconf=NIDiff, minval=0.0, maxval=0.0,
+
+
+"""
+`daqaddinput(dev::NIDev, chans, channames; termconf, minval, maxmval, units, customscale)`
+
+Adds input to `NIDev` object. For now it handles only AI Voltage channels. In the future, 
+more specific input types will be implemented.
+
+## Arguments
+ * `chans`: Physical channels usually something like "Dev1/ai0". See NIDAQmx documentation.
+ * `channames`: Channel names, if "" the physical names are used. 
+ * `termconf`: Terminal configuration (RSE, NRSE, Differential, Pseudodifferential). See [`TermConf`] and NIDAQmx docs for further information
+ * `minval` and `maxval`:
+
+"""
+function AbstractDAQs.daqaddinput(dev::NIDev, chans::AbstractString, channames::AbstractString="";
+                     termconf=NIDefault, minval=0.0, maxval=5.0,
                      units=NIDAQ.DAQmx_Val_Volts, customscalename="")
     r = NIDAQ.DAQmxCreateAIVoltageChan(dev.handle, chans, channames,
                                        termconf, minval, maxval,
@@ -81,8 +150,8 @@ function daqaddinput(dev::NIDev, chans::AbstractString, channames::AbstractStrin
     return 
 end
 
-function daqaddinput(dev::NIDev, devname::AbstractString,
-                     chans::AbstractVector, channames="";
+function AbstractDAQs.daqaddinput(dev::NIDev, devname::AbstractString,
+                     chans::AbstractVector{<:Int}, channames="";
                      termconf=NIDiff, s = "ai",
                      minval=0.0, maxval=0.0,
                      units=NIDAQ.DAQmx_Val_Volts, customscalename="")
@@ -113,7 +182,7 @@ function daqaddinput(dev::NIDev, devname::AbstractString,
         
 end
 
-function numchannels(dev::NIDev)
+function AbstractDAQs.numchannels(dev::NIDev)
     num = zeros(UInt32, 1)
     r = NIDAQ.DAQmxGetTaskNumChans(dev.handle, num)
 
@@ -123,7 +192,7 @@ function numchannels(dev::NIDev)
     
 end
 
-function daqchannels(dev::NIDev)
+function AbstractDAQs.daqchannels(dev::NIDev)
     nch = numchannels(dev)
 
     nperch = 30 # Number of characters per channel.
@@ -149,20 +218,67 @@ function daqconfig(dev::NIDev; rate=100.0, nsamples=1,
 
     r = NIDAQ.DAQmxCfgSampClkTiming(dev.handle, source, rate,
                                     activeedge, samplemode, nsamples)
-    
     r != 0 && throw(NIException(r))
+
+    dev.freq = freq
+    dev.nsamples = nsamples
 
     return 
                                     
 end                       
 
-function isdaqfinished(dev::NIDev)
-#    b = zeros(In
+function AbstractDAQs.isdaqfinished(dev::NIDev)
+
+    data = zeros(Int32,1)
+    #data = reinterpret(NIDAQ.Bool32, zeros(Int32, 1))
+    
+    r = NIDAQ.DAQmxIsTaskDone(dev.handle, data)
+    r != 0 && throw(NIException(r))
+    return data[1] != 0
+end
+
+AbstractDAQs.isreading(dev::NIDev) = !isdaqfinished(dev)
+
+    
+function AbstractDAQs.daqstart(dev::NIDev, usethread=true)
+    r = NIDAQ.DAQmxStartTask(dev.handle)
+    r != 0 && throw(NIException(r))
+    return
+end
+
+
+function AbstractDAQs.daqread(dev::NIDev)
+
+    nch = numchannels(dev)
+    nsamples = dev.nsamples
+    buffer = zeros(Float64, nch, nsamples)
+    fm1 =  Int32[NIDAQ.DAQmx_Val_GroupByScanNumber]
+    fm =  reinterpret(NIDAQ.Bool32, fm1)[1]
+    nsr = Int32[1]
+    r = NIDAQ.DAQmxReadAnalogF64(dev.handle, -1, -1.0, fm, buffer, nsamples*nch, nsr,
+                                 Ptr{NIDAQ.Bool32}(0))
+
+    r != 0 && throw(NIException(r))
+
+    stoptask(dev)
+    
+    return buffer, dev.freq
     
 end
 
-function daqstart(dev::NIDev, usethread=true)
-    
+function AbstractDAQs.daqacquire(dev::NIDev)
+    daqstart(dev)
+    return daqread(dev)
 end
 
+function AbstractDAQs.samplesread(dev::NIDev)
+
+    data = zeros(UInt64,1)
+    r = NIDAQ.DAQmxGetReadTotalSampPerChanAcquired(dev.handle, data)
+    r != 0 && throw(NIException(r))
+
+    return Int64(data[1])
 end
+
+
+end # module DAQnidaqmx
